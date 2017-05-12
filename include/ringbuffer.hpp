b89625338308b1d495604b438fb0b986b858f503
Max <savagecm@users.noreply.github.com>
2017-05-12 14:34:53 +0800

    Update ringbuffer.hpp

--- include/ringbuffer.hpp
+++ include/ringbuffer.hpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016-20017 Max Cong <savagecm@qq.com>
- * this code can be found at https://github.com/maxcong001/ring_buffer.git
+ * this code can be found at https://github.com/maxcong001/ring_buffer_flat.git
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
